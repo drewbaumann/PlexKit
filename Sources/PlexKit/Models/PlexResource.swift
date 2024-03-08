5390c629642c6f6cb0486b52850b2f66fb6d8ce3
Drew Baumann <drewbaumann@gmail.com>
2024-03-08 09:02:40 -0800

    refactor: Makes PlexResource#id a string

--- Sources/PlexKit/Models/PlexResource.swift
+++ Sources/PlexKit/Models/PlexResource.swift
@@ -10,13 +10,13 @@ import Foundation
 
 @dynamicMemberLookup
 public struct PlexResource: Codable, Identifiable, Hashable {
-    public let id: UUID // Add an id property to conform to Identifiable
+    public let id: String // Add an id property to conform to Identifiable
     private let model: PlexResourceModel
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
         self.model = try container.decode(PlexResourceModel.self)
-        self.id = UUID() // Generate a unique UUID for each PlexResource instance
+        self.id = UUID().uuidString // Generate a unique UUID for each PlexResource instance
     }
 
     public func encode(to encoder: Encoder) throws {
